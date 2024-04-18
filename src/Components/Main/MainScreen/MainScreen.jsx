e9b0923d584d104eaeab488b7760c7d8cbda4bfe
HannaTsilikina <https://github.com/HannaTsilikina/Practicum17.git>
2024-04-19 00:37:11 +0300

    new styles for MainScreen were applied

--- src/Components/Main/MainScreen/MainScreen.jsx
+++ src/Components/Main/MainScreen/MainScreen.jsx
@@ -2,13 +2,53 @@ import MainDepartments from "./MainDepartments/MainDepartments";
 import MainProfessions from "./MainProfessions/MainProfessions";
 import "./MainScreen.scss";
 import MainUsers from "./MainUsers/MainUsers";
+import data from "../../../../data.json";
 
 const MainScreen = () => {
+  const members = data.members;
+
+  const shuffleArray = (array) => {
+    const shuffledArray = [...array];
+    for (let i = shuffledArray.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [shuffledArray[i], shuffledArray[j]] = [
+        shuffledArray[j],
+        shuffledArray[i],
+      ];
+    }
+    return shuffledArray;
+  };
+
+  const shuffledMembers = shuffleArray(members);
+  const selectedMembers = shuffledMembers.slice(0, 10);
+  const selectedMembersID = selectedMembers.map((item) => item.id);
+  const uniqueCompaniesID = new Set(
+    selectedMembers.flatMap((member) =>
+      member.companies.map((company) => company.id)
+    )
+  );
+  const uniqueCompanies = data.companies.map((company) => {
+    {
+      if (data.companies.id === uniqueCompaniesID.value) return company.name;
+    }
+  });
+  const uniqueDirectionsID = new Set(
+    selectedMembers.flatMap((member) =>
+      member.directions.map((direction) => direction.id)
+    )
+  );
+  const uniqueDirections = data.directions.map((direction) => {
+    {
+      if (data.directions.id === uniqueDirectionsID.value)
+        return direction.name;
+    }
+  });
+
   return (
     <main className="mainscreen__main">
-      <MainDepartments />
-      <MainUsers />
-      <MainProfessions />
+      <MainDepartments arrayOfCompanies={uniqueCompanies} />
+      <MainUsers arrayOfMembers={selectedMembersID} />
+      <MainProfessions arrayOfProfessions={uniqueDirections} />
     </main>
   );
 };
--- src/Components/Main/MainScreen/MainUsers/MainUsers.jsx
+++ src/Components/Main/MainScreen/MainUsers/MainUsers.jsx
@@ -2,6 +2,7 @@ import { Link } from "react-router-dom";
 import "./MainUsers.scss";
 import "./../MainScreen.scss";
 import user from "../../../../assets/images/members/member.png";
+import { useState, useEffect } from "react";
 
 const arrayOfUsers = [
   user,
@@ -15,37 +16,88 @@ const arrayOfUsers = [
   user,
   user,
 ];
-const StylePosition = () => {
-  const arraysOfPosition = [
-    "position-left-start",
-    "position-left-end",
-    "position-center",
-    "position-right-end",
-    "position-right-start",
-  ];
 
-  const randomIntFromInterval = (min, max) => {
-    return Math.floor(Math.random() * (max - min + 1) + min);
-  };
-  const index = randomIntFromInterval(0, 4);
+const MainUsers = ({ arrayOfMembers }) => {
+  const [positions, setPositions] = useState([]);
+  const [isIntersectingThreshold, setIsIntersectingThreshold] = useState({
+    top: 0,
+    left: 0,
+  });
 
-  return arraysOfPosition[index];
-};
-const MainUsers = () => {
+  useEffect(() => {
+    const updateIntersectingThreshold = () => {
+      if (window.innerWidth >= 768 && window.innerWidth < 1024) {
+        setIsIntersectingThreshold({ top: 18, left: 21 });
+      } else if (window.innerWidth >= 1024 && window.innerWidth <= 1366) {
+        setIsIntersectingThreshold({ top: 15, left: 18 });
+      } else if (window.innerWidth >= 1366 && window.innerWidth <= 1920) {
+        setIsIntersectingThreshold({ top: 19, left: 20 });
+      }
+    };
+
+    updateIntersectingThreshold();
+
+    const handleResize = () => {
+      updateIntersectingThreshold();
+    };
+
+    window.addEventListener("resize", handleResize);
+
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
+
+  useEffect(() => {
+    const maxAttemptsPerElement = 35;
+    const maxElements = 10;
+    const maxAttempts = maxAttemptsPerElement * maxElements;
+
+    const generateUniquePositions = (maxAttempts) => {
+      const newPositions = [];
+      for (let i = 0; i < maxElements; i++) {
+        let attempts = 0;
+        let newPosition;
+        let isIntersecting;
+        do {
+          newPosition = {
+            top: `${Math.random() * 90}%`,
+            left: `${Math.random() * 90}%`,
+          };
+          isIntersecting = newPositions.some(
+            ({ top, left }) =>
+              Math.abs(parseFloat(top) - parseFloat(newPosition.top)) <
+                isIntersectingThreshold.top &&
+              Math.abs(parseFloat(left) - parseFloat(newPosition.left)) <
+                isIntersectingThreshold.left
+          );
+          attempts++;
+        } while (isIntersecting && attempts < maxAttempts);
+        if (!isIntersecting) {
+          newPositions.push(newPosition);
+        }
+      }
+      return newPositions;
+    };
+
+    const initialPositions = generateUniquePositions(maxAttempts);
+    setPositions(initialPositions);
+  }, [isIntersectingThreshold]);
+
   return (
     <div className="mainscreen-main__workers">
-      {arrayOfUsers.map((item, index) => {
-        let position = StylePosition();
-        return (
-          <Link key={index}>
-            <img
-              className={`mainscreen__user ${position} `}
-              src={user}
-              alt="image-user"
-            />
-          </Link>
-        );
-      })}
+      {arrayOfUsers.map((user, index) => (
+        <Link
+          key={index}
+          className="sp__container-result-users-user"
+          style={{
+            top: positions[index] ? positions[index].top : "0%",
+            left: positions[index] ? positions[index].left : "0%",
+          }}
+        >
+          <img src={user} alt="image-user" />
+        </Link>
+      ))}
     </div>
   );
 };
