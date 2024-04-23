21f8d83e541166b75438942fdd598555b41d14a8
lilibeth66 <lizi1056@gmail.com>
2024-04-23 10:56:30 +0300

    Update CompaniesAndDirections.jsx
    1

--- src/componentsCompaniesAndDirections/CompaniesAndDirections.jsx
+++ src/componentsCompaniesAndDirections/CompaniesAndDirections.jsx
@@ -3,104 +3,100 @@ import data from '../../data.json';
 import './CompaniesAndDirections.scss';
 
 
-const CompaniesAndDirections = () => {
-    const [selectedCompanyId, setSelectedCompanyId] = useState();
-    const [selectedDirectionId, setselectedDirectionId] = useState();
+/*
 
+const CompanySelect = ({ companies, onChange }) => (
+    <select onChange={onChange}>
+        {companies.map((company) => (
+            <option key={company.id} value={company.id}>
+                {company.name}
+            </option>
+        ))}
+    </select>
+);
 
-    const CompanySelect = ({ companies, onChange }) => (
-        <select onChange={onChange}>
-            {companies.map((company) => (
-                <option key={company.id} value={company.id}>
-                    {company.name}
-                </option>
+const DirectionSelect = ({ directions, onChange }) => (
+    <select onChange={onChange}>
+        {directions.map((direction) => (
+            <option key={direction.id} value={direction.id}>
+                {direction.name}
+            </option>
+        ))}
+    </select>
+);
+
+const MembersList = ({ members }) => {
+    return (
+        <div>
+            {members.map((member) => (
+                <div key={member.id}>
+                    <p>
+                        {member.firstName} {member.lastName}
+                    </p>
+                </div>
             ))}
-        </select>
+        </div>
     );
+};
 
-    const DirectionSelect = ({ directions, onChange }) => (
-        <select onChange={onChange}>
-            {directions.map((direction) => (
-                <option key={direction.id} value={direction.id}>
-                    {direction.name}
-                </option>
-            ))}
-        </select>
-    );
+export default function CompaniesAndDirections() {
 
-    const MembersList = ({ members }) => {
-        return (
-            <div>
-                {members.map((member) => (
-                    <div key={member.id}>
-                        <p>
-                            {member.firstName} {member.lastName}
-                        </p>
-                    </div>
-                ))}
-            </div>
-        );
+    const [selectedCompanyId, setSelectedCompanyId] = useState(null);
+    const [selectedDirectionId, setselectedDirectionId] = useState(null);
+
+
+    const handleCompanyChange = (e) => {
+        setSelectedCompanyId(parseInt(e.target.value));
     };
 
-    export default function CompaniesAndDirections() {
+    const handleDirectionChange = (e) => {
+        setselectedDirectionId(parseInt(e.target.value));
+    };
 
-        const [selectedCompanyId, setSelectedCompanyId] = useState(null);
-        const [selectedDirectionId, setselectedDirectionId] = useState(null);
+    const filteredMembersCompany = data.members.filter(
+        (member) =>
+            !selectedCompanyId ||
+            member.companies.some(
+                (company) => company.id === selectedCompanyId && company.status === "Работает"
+            ));
 
+    const filteredMembersDirection = data.members.filter(
+        (member) =>
+            !selectedDirectionId ||
+            member.directions.some(
+                (direction) => direction.id === selectedDirectionId
+            ));
 
-        const handleCompanyChange = (e) => {
-            setSelectedCompanyId(parseInt(e.target.value));
-        };
-
-        const handleDirectionChange = (e) => {
-            setselectedDirectionId(parseInt(e.target.value));
-        };
-
-        const filteredMembersCompany = data.members.filter(
-            (member) =>
-                !selectedCompanyId ||
-                member.companies.some(
-                    (company) => company.id === selectedCompanyId && company.status === "Работает"
-                ));
-
-        const filteredMembersDirection = data.members.filter(
-            (member) =>
-                !selectedDirectionId ||
-                member.directions.some(
-                    (direction) => direction.id === selectedDirectionId
-                ));
-
-        return (
-            <div>
-                <div className='cad__container'>
-                    <div className='cad__container-companies'>
-                        <h2>Компания:</h2>
-                        <option value="">Выберите компанию</option>
-                        <CompanySelect
-                            companies={data.companies}
-                            onChange={handleCompanyChange} /></div>
-                </div>
-
-
-                <div>
-                    <h2>Направление:</h2>
-                    <option value="">Выберите направление</option>
-                    <DirectionSelect
-                        directions={data.directions}
-                        onChange={handleDirectionChange} />
-                </div>
-
-                <div>
-                    <h2>Работники Компании:</h2>
-                    <MembersList members={filteredMembersCompany} />
-                </div>
-
-                <div className='cad__container-members-directions'>
-
-                    <MembersList members={filteredMembersDirection} />
-                </div>
+    return (
+        <div>
+            <div className='cad__container'>
+                <div className='cad__container-companies'>
+                    <h2>Компания:</h2>
+                    <option value="">Выберите компанию</option>
+                    <CompanySelect
+                        companies={data.companies}
+                        onChange={handleCompanyChange} /></div>
             </div>
-        </div >
+
+
+            <div>
+                <h2>Направление:</h2>
+                <option value="">Выберите направление</option>
+                <DirectionSelect
+                    directions={data.directions}
+                    onChange={handleDirectionChange} />
+            </div>
+
+            <div>
+                <h2>Работники Компании:</h2>
+                <MembersList members={filteredMembersCompany} />
+            </div>
+
+            <div className='cad__container-members-directions'>
+
+                <MembersList members={filteredMembersDirection} />
+            </div>
+        </div>
     )
 
-};
+};*/
