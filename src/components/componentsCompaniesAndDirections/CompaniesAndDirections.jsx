8acaa14bbabe077ff76646ba826f24516e8bf176
lilibeth66 <lizi1056@gmail.com>
2024-05-03 22:04:04 +0300

    test1

--- src/components/componentsCompaniesAndDirections/CompaniesAndDirections.jsx
+++ src/components/componentsCompaniesAndDirections/CompaniesAndDirections.jsx
@@ -83,6 +83,20 @@ function Company_Directions_Page() {
 export default Company_Directions_Page;
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 /*
 import React from 'react';
 import data from '../../../data.json';
@@ -164,182 +178,4 @@ function CompaniesAndDirections(dataType) {
     )
 }
 
-export default CompaniesAndDirections;
-
-/*
-
-const CompanySelect = ({ companies, onChange }) => (
-    <select onChange={onChange}>
-        {companies.map((company) => (
-            <option key={company.id} value={company.id}>
-                {company.name}
-            </option>
-        ))}
-    </select>
-);
-
-const DirectionSelect = ({ directions, onChange }) => (
-    <select onChange={onChange}>
-        {directions.map((direction) => (
-            <option key={direction.id} value={direction.id}>
-                {direction.name}
-            </option>
-        ))}
-    </select>
-);
-
-const MembersList = ({ members }) => {
-    return (
-        <div>
-            {members.map((member) => (
-                <div key={member.id}>
-                    <p>
-                        {member.firstName} {member.lastName}
-                    </p>
-                </div>
-            ))}
-        </div>
-    );
-};
-
-  <h1> {dataType === "companies" ? companies : directions}</h1>
-                {dataToShow.map((company, direction) => (
-                    <div key={company.id}>
-                        <UniversalComponent name={company.name}>
-                        </UniversalComponent>
-                    </div>
-                )
-                )}
-            </div>
-
-export default function CompaniesAndDirections() {
-
-    const [selectedCompanyId, setSelectedCompanyId] = useState(null);
-    const [selectedDirectionId, setselectedDirectionId] = useState(null);
-
-
-    const handleCompanyChange = (e) => {
-        setSelectedCompanyId(parseInt(e.target.value));
-    };
-
-    const handleDirectionChange = (e) => {
-        setselectedDirectionId(parseInt(e.target.value));
-    };
-
-    const filteredMembersCompany = data.members.filter(
-        (member) =>
-            !selectedCompanyId ||
-            member.companies.some(
-                (company) => company.id === selectedCompanyId && company.status === "Работает"
-            ));
-
-    const filteredMembersDirection = data.members.filter(
-        (member) =>
-            !selectedDirectionId ||
-            member.directions.some(
-                (direction) => direction.id === selectedDirectionId
-            ));
-
-    return (
-        <div>
-            <div className='cad__container'>
-                <div className='cad__container-companies'>
-                    <h2>Компания:</h2>
-                    <option value="">Выберите компанию</option>
-                    <CompanySelect
-                        companies={data.companies}
-                        onChange={handleCompanyChange} /></div>
-            </div>
-
-
-            <div>
-                <h2>Направление:</h2>
-                <option value="">Выберите направление</option>
-                <DirectionSelect
-                    directions={data.directions}
-                    onChange={handleDirectionChange} />
-            </div>
-
-            <div>
-                <h2>Работники Компании:</h2>
-                <MembersList members={filteredMembersCompany} />
-            </div>
-
-            <div className='cad__container-members-directions'>
-
-                <MembersList members={filteredMembersDirection} />
-            </div>
-        </div>
-    )
-
-};*/
-
-
-
-
-
-
-/*
-import React from 'react';
-
-const usersData = require('./users.json');
-
-const UserList = ({ users, filterBy }) => {
-  const filteredUsers = users.filter(user => {
-    if (filterBy.type === 'company') {
-      return user.company === filterBy.value;
-    } else if (filterBy.type === 'direction') {
-      return user.direction === filterBy.value;
-    }
-    return true; // Показать всех пользователей, если нет фильтра
-  });
-
-  return (
-    <div>
-      {filteredUsers.map(user => (
-        <div key={user.id}>
-          <p>{user.name}</p>
-          <p>{user.company}</p>
-          <p>{user.direction}</p>
-        </div>
-      ))}
-    </div>
-  );
-};
-
-const App = () => {
-  const users = usersData.filter(item => item.type === 'user').map(item => item.data);
-  
-  const companyFilter = { type: 'company', value: 'Example Company' };
-  const directionFilter = { type: 'direction', value: 'Example Direction' };
-
-  return (
-    <div>
-      <h2>Users from Company</h2>
-      <UserList users={users} filterBy={companyFilter} />
-
-      <h2>Users from Direction</h2>
-      <UserList users={users} filterBy={directionFilter} />
-    </div>
-  );
-};
-
-export default App;
-*/
-
-
-/* const title = ({ dataType }) => {
-         // json файл содержит объект с ключами 'companies' и 'directions'
-         const { companies, directions } = data;
- 
-         let dataToShow;
- 
-         // Определяем, какие данные нужно отобразить в зависимости от входного параметра
-         if (dataType === 'company') {
-             dataToShow = companies;
-         } else if (dataType === 'direction') {
-             dataToShow = directions;
-         } else {
-             return <div>Неправильный тип данных</div>;
-         }
-     }*/
+export default CompaniesAndDirections;*/
