cb4b0ca17167fc942ab2f63239eabdc9d1cd196f
HannaTsilikina <https://github.com/HannaTsilikina/Practicum17.git>
2024-05-03 22:19:45 +0300

    commit

--- src/Pages/App.jsx
+++ src/Pages/App.jsx
@@ -1,14 +1,19 @@
 import "../style/App.scss";
 import Header from "../Components/Header/Header";
 import MainScreen from "../Components/Main/MainScreen/MainScreen";
+import SearchingPage from '../Pages/SearchingPage/SearchingPage';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 function App() {
-  return (
-    <>
-      <Header />
-      <MainScreen />
-    </>
-  );
+    return (
+        <Router>
+            <Header />
+            <Routes>
+                <Route path='/' element={<MainScreen />} />
+                <Route path='/search' element={<SearchingPage />} />
+            </Routes>
+        </Router>
+    );
 }
 export default App;
 
